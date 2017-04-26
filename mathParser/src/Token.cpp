6e98134c407a8991b6c0df24db42fa895dbbdeeb
Olivier Lischer <liolindeb@gmail.com>
2017-04-26 18:31:19 +0200

    Edit Token logic and files

--- mathParser/src/Token.cpp
+++ mathParser/src/Token.cpp
@@ -5,6 +5,13 @@ using namespace BasicParser;
 Token::Token(TokenType type)
 {
     this->SetType(type);
+    this->SetValue(0);
+}
+
+Token::Token(TokenType type, int value)
+{
+    this->SetType(type);
+    this->SetValue(value);
 }
 
 void Token::SetType(TokenType type)
@@ -15,4 +22,14 @@ void Token::SetType(TokenType type)
 TokenType Token::GetType()
 {
     return this->type;
+}
+
+void Token::SetValue(int value)
+{
+    this->value = value;
+}
+
+int Token::GetValue()
+{
+    return this->value;
 }
--- mathParser/src/Token.h
+++ mathParser/src/Token.h
@@ -4,26 +4,38 @@
 namespace BasicParser
 {
     enum TokenType {
+	// general tokens
 	T_NIL    = 0,
 	T_BEGIN  = 1,
 	T_END    = 2,
 	T_ERROR  = 3,
-	T_NUMBER = 10, 
-	T_ADD    = 11,
-	T_MIN    = 12,
-	T_MUL    = 13,
-	T_DIV    = 14
+
+	// number tokens
+	T_INT    = 10,
+	T_FLOUT  = 11,
+
+	// operators
+	T_ADD    = 20,
+	T_MIN    = 21,
+	T_MUL    = 22,
+	T_DIV    = 23,
+	T_EQUAL  = 24
     };
+
 
     class Token
     {
     protected:
 	TokenType type;
-
+	int value;
+	
     public:
 	Token(TokenType type);
+	Token(TokenType type, int value);
 	void SetType(TokenType type);
 	TokenType GetType();
+	void SetValue(int value);
+	int GetValue();
     };
 }
 #endif /* TOKEN_H */
